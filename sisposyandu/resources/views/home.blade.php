ce836430e6cc354f282b13fd7822616250d829b0
billyindrai <58163021+billyindrai@users.noreply.github.com>
2020-06-12 23:31:48 +0700

    nambahin home

--- sisposyandu/resources/views/home.blade.php
+++ sisposyandu/resources/views/home.blade.php
@@ -11,10 +11,8 @@
 <body>
     @yield('header')
     <header>
-        <div class="jumbotron">
             <h1 style="font-size: 34px; margin-bottom: 20px;">SCPK Puskesmas Maju Jaya</h1>
             <p>Membantu penatalaksanaan lansia dengan keluhan tertentu</p>
-        </div>
         <nav>
             <a href="/">Input Data</a>
             <a href="/riwayat">Data Lansia</a>
