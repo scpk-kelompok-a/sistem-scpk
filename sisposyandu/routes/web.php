b5107e2a63262c310ff9e1d6c4672bbf43b3cca5
Reza Azerly <kiritosao080@gmail.com>
2020-06-13 12:52:28 +0700

    Update web.php

--- sisposyandu/routes/web.php
+++ sisposyandu/routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/riwayat', 'KMSController@index');
 
-Route::get('/index', function () {
+Route::get('/', function () {
     return view('index');
 });
